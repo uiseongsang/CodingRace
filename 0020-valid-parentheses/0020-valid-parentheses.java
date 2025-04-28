931493a78202bee97d467734a54edf975d6adb8d
Thomas <40707686+uiseongsang@users.noreply.github.com>
2025-04-29 00:12:21 +0900

    Time: 2 ms (97.39%), Space: 41.8 MB (47.95%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++ ) {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if(st.empty()) {
+                    return false;
+                } else {
+                    char cmpCh = st.peek();
+
+                    if ((ch == ')' && cmpCh == '(') ||
+                    (ch == '}' && cmpCh == '{') ||
+                    (ch == ']' && cmpCh == '[')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+                }
+            }
+        }
+
+        return st.empty();
+    }
+}
