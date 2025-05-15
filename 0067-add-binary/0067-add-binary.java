5baa34e389ea35911e1aada2a3d70a06a8e9418f
Thomas <40707686+uiseongsang@users.noreply.github.com>
2025-05-15 23:14:00 +0900

    Time: 6 ms (13.19%), Space: 42.4 MB (38.84%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,8 +1,10 @@
+import java.math.BigInteger;
+
 class Solution {
     public String addBinary(String a, String b) {
-        int ia = Integer.parseInt(a, 2);  // 2진수 문자열 a -> 10진수 정수
-        int ib = Integer.parseInt(b, 2);  // 2진수 문자열 b -> 10진수 정수
-        int sum = ia + ib;                
-        return Integer.toBinaryString(sum); // 10진수 -> 2진수 문자열
+        BigInteger ia = new BigInteger(a, 2); 
+        BigInteger ib = new BigInteger(b, 2);
+        BigInteger sum = ia.add(ib);
+        return sum.toString(2);
     }
 }
